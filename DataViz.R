8888f8cd741faf338f9ec9428d0b41375cdc1a63
Anish Parepalli <64266870+aparep7474@users.noreply.github.com>
2024-01-09 14:17:43 -0500

    Update DataViz.R

--- DataViz.R
+++ DataViz.R
@@ -3,6 +3,10 @@ library(dplyr)
 library(ggplot2)
 library(ggrepel)
 library(extrafont)
+library(deldir)
+library(polyclip)
+library(gganimate)
+library(utils)
 
 
 #REPEAT FOR PASS HEATMAP
@@ -80,7 +84,296 @@ ggplot(importance, aes(x = factorName %% 10, y = factorName %/% 10, fill = Relat
 
 
 
+# Animated Voronoi and TT Graphs
+# Animated Expected Coords
+
+# Results from Voronoi Box, the area around the ball carrier
+x_pos_run <- 9
+x_neg_run <- 3
+y_run <- 5
+
+x_pos_pass <- 9
+x_neg_pass <- 4
+y_pass <- 6
+
+WINDOWX1 <- 0
+WINDOWX2 <- 0
 
 
 
 
+
+voronoi_visual <- function(play, play_type) {
+  x_pos <- get(paste0("x_pos_", play_type))
+  x_neg <- get(paste0("x_neg_", play_type))
+  y_s <- get(paste0("y_", play_type))
+  
+  play$type <- play$club
+  play$type[play$ballCarrier == 1] = "Ball Carrier"
+  
+  play$poly_id <- 1:nrow(play)
+  
+  frames <- split(play, play$frameId)
+  updated_frames <- list()
+  for (f in seq_along(frames)) {
+    field <- frames[[f]][1:22, ]
+    
+    ball_carrier <- field[field$ballCarrier == 1, ]
+    field$x_min <- ball_carrier[1, "x_std"] - x_neg
+    field$x_max <- ball_carrier[1, "x_std"] + x_pos
+    field$y_min <- ball_carrier[1, "y_std"] - y_s
+    field$y_max <- ball_carrier[1, "y_std"] + y_s
+    
+    tess <- deldir(field$x_std, field$y_std, rw=c(-20, 140, -10, 63.3))
+    tiles <- tile.list(tess)
+    
+    updated_field <- list()
+    for (i in 1:22) {
+      poly_x = as.numeric(tiles[[i]]$x)
+      poly_y = as.numeric(tiles[[i]]$y)
+      updated_field[[i]] <- field[i, ] %>%
+        merge(as.data.frame(poly_x)) %>%
+        mutate(poly_y = poly_y)
+    }
+    bind_rows(updated_field)
+    
+    updated_frames[[f]] <- updated_field
+  }
+  updated_play <- bind_rows(updated_frames)
+  
+  plot <- ggplot() +
+    geom_polygon(data = updated_play, aes(x = poly_y,
+                                          y = poly_x,
+                                          group = poly_id,
+                                          fill = type),
+                 color = "black") +
+    scale_fill_manual(values = c("Ball Carrier" = "green", "TeamName1" = "TeamColor1", "TeamName2" = "TeamColor2"))
+  
+  plot <- plot +
+    geom_rect(data = updated_play, aes(xmin = y_min, xmax = y_max, ymin = x_min, ymax = x_max),
+              fill = "transparent", color = "green")
+  
+  plot <- plot + 
+    geom_point(data = play, aes(y_std, x_std), size = 1, color='white') +
+    scale_x_reverse() +
+    # CHANGE HARD CODED X AND Y WHEN CHANGING PLAY
+    coord_fixed(xlim = c(53.3, 0), ylim = c(WINDOWX1, WINDOWX2)) +
+    theme(panel.background = element_blank(),
+          panel.grid = element_blank(),
+          axis.title = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank())
+  
+  plot <- plot + 
+    transition_time(frameId)
+  
+  plot_anim <- animate(plot, fps = 10)
+  
+  return(plot_anim)
+}
+
+future_player_visual <- function(play, fut_play) {
+  play$type <- play$club
+  play$type[play$ballCarrier == 1] = "Ball Carrier"
+  play$type[play$nflId == fut_play$nflId[1]] = "Player"
+  
+  plot <- ggplot(play, aes(x = y_std, y = x_std)) + 
+    geom_point(data = play, aes(x = y_std, y = x_std, color = type), size = 2) +
+    geom_point(data = fut_play, aes(x = predY, y = predX), size = 2, color = "TeamColor2", shape = 21) +
+    scale_color_manual(values = c("Player" = "TeamColor2", "Ball Carrier" = "green")) +
+    scale_x_reverse() +
+    # CHANGE HARD CODED X AND Y WHEN CHANGING PLAY
+    coord_fixed(xlim = c(53.3, 0), ylim = c(WINDOWX1, WINDOWX2)) +
+    theme(panel.background = element_blank(),
+          panel.grid = element_blank(),
+          axis.title = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank())
+  
+  plot <- plot + 
+    transition_time(frameId)
+  
+  plot_anim <- animate(plot, fps = 10)
+  
+  return(plot_anim)
+}
+
+ttt_visual <- function(play) {
+  plot <- ggplot(play, aes(x = frameId, y = ttt)) +
+    geom_line() + 
+    xlim(0, play[nrow(play), "frameId"]) +
+    xlab("Frames After Motion") +
+    ylab("Time to Tackle Prediction")
+  
+  plot <- plot +
+    transition_reveal(frameId)
+  
+  plot_anim <- animate(plot, start_pause = play[1, "frameId"]-1, fps = 10)
+  
+  return(plot_anim)
+}
+
+player_id <- player_id
+game_id <- game_id
+play_id <- play_id
+
+plot_fut_play <- final_run_data %>%
+  filter(gameId == game_id & playId == play_id & nflId == player_id) %>%
+  arrange(frameId)
+
+plot_play <- tracking_week_8 %>%
+  filter(gameId == game_id & playId == play_id) %>%
+  filter(club != "football") %>%
+  arrange(frameId) %>%
+  filter(frameId <= plot_fut_play$tackleFrame[1])
+
+plot_play_ttt <- ttt_run_pred %>%
+  filter(gameId == game_id & playId == play_id)
+plot_play_ttt <- plot_play %>%
+  filter(frameId %in% plot_play_ttt$frameId) %>%
+  filter(nflId == player_id) %>%
+  mutate(ttt = plot_play_ttt$timeToTacklePred)
+
+plot_i <- future_player_visual(plot_play, plot_fut_play)
+plot_ii <- voronoi_visual(plot_play, play_type)
+plot_iv <- ttt_visual(plot_play_ttt)
+plot_i
+plot_ii
+plot_iv
+
+
+
+
+# Static Voronoi and TT Graphs
+
+static_frame <- 0
+
+voronoi_visual_static <- function(play, play_type, highlight) {
+  x_pos <- get(paste0("x_pos_", play_type))
+  x_neg <- get(paste0("x_neg_", play_type))
+  y_s <- get(paste0("y_", play_type))
+  
+  play$type <- play$club
+  play$type[play$ballCarrier == 1] = "Ball Carrier"
+  play$type[play$nflId == highlight] = "Player"
+  
+  play$poly_id <- 1:nrow(play)
+  
+  frames <- split(play, play$frameId)
+  updated_frames <- list()
+  for (f in seq_along(frames)) {
+    field <- frames[[f]][1:22, ]
+    
+    ball_carrier <- field[field$ballCarrier == 1, ]
+    field$x_min <- ball_carrier[1, "x_std"] - x_neg
+    field$x_max <- ball_carrier[1, "x_std"] + x_pos
+    field$y_min <- ball_carrier[1, "y_std"] - y_s
+    field$y_max <- ball_carrier[1, "y_std"] + y_s
+    
+    tess <- deldir(field$x_std, field$y_std, rw=c(-20, 140, -10, 63.3))
+    tiles <- tile.list(tess)
+    
+    updated_field <- list()
+    for (i in 1:22) {
+      poly_x = as.numeric(tiles[[i]]$x)
+      poly_y = as.numeric(tiles[[i]]$y)
+      updated_field[[i]] <- field[i, ] %>%
+        merge(as.data.frame(poly_x)) %>%
+        mutate(poly_y = poly_y)
+    }
+    bind_rows(updated_field)
+    
+    updated_frames[[f]] <- updated_field
+  }
+  updated_play <- bind_rows(updated_frames)
+  
+  play <- play %>%
+    filter(frameId == static_frame)
+  updated_play <- updated_play %>%
+    filter(frameId == static_frame)
+  
+  plot <- ggplot() +
+    geom_polygon(data = updated_play, aes(x = poly_y,
+                                          y = poly_x,
+                                          group = poly_id,
+                                          fill = type),
+                 color = "black") +
+    scale_fill_manual(values = c("Player" = "#6ea2ff", "Ball Carrier" = "green", "TeamName1" = "TeamColor1", "TeamName2" = "TeamColor2"))
+  
+  plot <- plot +
+    geom_rect(data = updated_play, aes(xmin = y_min, xmax = y_max, ymin = x_min, ymax = x_max),
+              fill = "transparent", color = "green")
+  
+  plot <- plot + 
+    geom_point(data = play, aes(y_std, x_std), size = 1, color='white') +
+    scale_x_reverse() +
+    # CHANGE HARD CODED X AND Y WHEN CHANGING PLAY
+    coord_fixed(xlim = c(53.3, 0), ylim = c(WINDOWX, WINDOWY)) +
+    theme(panel.background = element_blank(),
+          panel.grid = element_blank(),
+          axis.title = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank()) +
+    ggtitle("Frame #")
+  
+  return(plot)
+}
+
+ttt_visual_static <- function(play) {
+  play <- play %>%
+    mutate(frame = ifelse(frameId == static_frame, 1, 0))
+  
+  plot <- ggplot(play, aes(x = frameId, y = ttt, color = frame)) +
+    geom_line() + 
+    geom_point(size = 3) +
+    theme(legend.position="none") +
+    xlab("Frames After Motion") +
+    ylab("Time to Tackle Prediction")
+  
+  return(plot)
+}
+
+player_id <- player_id
+game_id <- game_id
+play_id <- play_id
+
+plot_fut_play <- final_run_data %>%
+  filter(gameId == game_id & playId == play_id & nflId == player_id) %>%
+  arrange(frameId)
+
+plot_play <- tracking_week_8 %>%
+  filter(gameId == game_id & playId == play_id) %>%
+  filter(club != "football") %>%
+  arrange(frameId) %>%
+  filter(frameId <= plot_fut_play$tackleFrame[1])
+
+plot_play_r <- plot_play %>%
+  filter(frameId %in% plot_fut_play$frameId) %>%
+  filter(nflId == player_id) %>%
+  mutate(x_std = plot_fut_play$predX) %>%
+  mutate(y_std = plot_fut_play$predY)
+plot_play_r <- rows_update(plot_play, plot_play_r, by = c("nflId", "frameId"))
+
+plot_play_ttt <- ttt_run_pred %>%
+  filter(gameId == game_id & playId == play_id)
+plot_play_ttt <- plot_play %>%
+  filter(frameId %in% plot_play_ttt$frameId) %>%
+  filter(nflId == player_id) %>%
+  mutate(ttt = plot_play_ttt$timeToTacklePred)
+
+plot_play_ttt_exp <- ttt_run_pred_exp %>%
+  filter(gameId == game_id & playId == play_id & pred_player == player_id)
+plot_play_ttt_exp <- plot_play %>%
+  filter(frameId %in% plot_play_ttt_exp$frameId) %>%
+  filter(nflId == player_id) %>%
+  mutate(ttt = plot_play_ttt_exp$timeToTacklePred)
+
+plot_ii <- voronoi_visual_static(plot_play, play_type, player_id)
+plot_iii <- voronoi_visual_static(plot_play_r, play_type, player_id)
+plot_iv <- ttt_visual_static(plot_play_ttt)
+plot_v <- ttt_visual_static(plot_play_ttt_exp)
+plot_ii
+plot_iii
+plot_iv
+plot_v
+ 
